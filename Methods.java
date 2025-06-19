e300f43199a89ef7e5571c47c4a7da78536bc524
Namburu Rama Sathwik <nambururama17@gmail.com>
2025-06-19 11:42:35 +0530

    Create Methods.java

---
+++ Methods.java
@@ -0,0 +1,56 @@
+/*class Methods
+{
+ public int sub()
+ {
+    return(10-5);
+ }
+ public static void main(String []args)
+ {
+  Methods m=new Methods();
+  int b=m.sub();
+  System.out.println(b);
+ }
+}*/
+
+/*class Methods
+{
+ public void add()
+ {
+   System.out.println(10+20);
+ }
+ public static void main(String []args)
+ {
+  Methods m=new Methods();
+  m.add();
+ }
+}*/
+
+
+/*class Methods
+{
+ public void mul(int a,int b)
+ {
+   System.out.println(a*b);
+ }
+ public static void main(String []args)
+ {
+  Methods m=new Methods();
+  m.mul(10,20);
+ }
+}*/
+
+
+/*class Methods
+{
+ public int div(int a,int b)
+ {
+   int c=a/b;
+   return(c);
+ }
+ public static void main(String []args)
+ {
+  Methods m=new Methods();
+  int d=m.div(20,2);
+  System.out.println(d);
+ }
+}*/
